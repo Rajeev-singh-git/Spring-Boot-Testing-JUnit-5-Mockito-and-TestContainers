0ab3295db069bb3d2eb1ab524df406948a181634
Rajeev Singh <rajeevsinghece49@gmail.com>
2024-12-06 14:15:15 +0530

    assertFalse Method

--- src/test/java/Service/StudentServiceTest.java
+++ src/test/java/Service/StudentServiceTest.java
@@ -11,20 +11,41 @@ class StudentServiceTest {
 
 
     @Test
-    public void getStudentsTest_whenListEmpty(){
+    public void getStudentsTestUsing_AssertTrue(){
         StudentService studentService = new StudentService();
         List<Student> listOfStudent = studentService.getStudents();
         boolean actualResult = listOfStudent.isEmpty();
 
 
         assertTrue(actualResult);
-        assertTrue(actualResult, "List of student is empty");  //Message will  be printed if assertion fails
+        assertTrue(actualResult, "List of student is not empty");  //Message will  be printed if assertion fails
 
         assertTrue(()->actualResult);  //Boolean Supplier interface
-        assertTrue(()->actualResult, "List of student is empty");  //Message will  be printed if assertion fails
+        assertTrue(()->actualResult, "List of student is not empty");  //Message will  be printed if assertion fails
+    }
 
+
+    @Test
+    public void getStudentsTestUsing_AssertFalse(){
+        StudentService studentService = new StudentService();
+
+        Student student = new Student(1,"Rajeev");
+        List<Student> listOfStudent = studentService.getStudents();
+
+        listOfStudent.add(student);
+
+        boolean actualResult = listOfStudent.isEmpty();
+
+        assertFalse(actualResult);
+        assertFalse(actualResult, "Student List must not be empty !!! ");
+
+        assertFalse(()->actualResult);
+        assertFalse(()->actualResult,"Student List must not be empty !!! ");
+
+        assertFalse(actualResult, () ->"Student List must not be empty !!! ");
 
     }
+
 
 
 
