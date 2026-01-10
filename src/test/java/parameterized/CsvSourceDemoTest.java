63aad7955820299b8d7110b7c13463567a788b67
Rajeev  Singh <rajeevsinghece49@gmail.com>
2026-01-10 13:42:40 +0530

    ParameterizedTest : @CsvSource

---
+++ src/test/java/parameterized/CsvSourceDemoTest.java
@@ -0,0 +1,25 @@
+package parameterized;
+
+import Controller.MathUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CsvSourceDemoTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1",
+            "1, 1",
+            "2, 2",
+            "3, 6",
+            "4, 24",
+            "5, 120"
+    })
+    void factorialTest(int number, int expected){
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(expected,mathUtils.factorial(number));
+    }
+
+}
