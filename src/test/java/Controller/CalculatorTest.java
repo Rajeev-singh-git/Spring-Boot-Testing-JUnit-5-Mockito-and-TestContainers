f1195fe4816a30f80225aa24b5cfed4262fb3b74
Rajeev  Singh <rajeevsinghece49@gmail.com>
2026-01-10 10:07:22 +0530

    Lifecycle annotation @Before Each

--- src/test/java/Controller/CalculatorTest.java
+++ src/test/java/Controller/CalculatorTest.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,32 +10,31 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("Calculator Operation Test")
 class CalculatorTest {
 
+    private Calculator calculator;
+
+    @BeforeEach
+    void setup(){
+        calculator = new Calculator();
+    }
+
     @Test
     @DisplayName("Test addition of Two Numbers")
     public void addTest(){
-
-        Calculator calculator = new Calculator();
         int actualResult  = calculator.add(10,20);
-
         assertEquals(30,actualResult);
     }
 
     @Test
     @DisplayName("Test Subtraction")
     void subtractTest(){
-        Calculator calculator = new Calculator();
         int subtraction = calculator.subtract(30,20);
-
         assertEquals(10,subtraction);
     }
 
 
     @Test
-    @Disabled("Disabled under bug fix")
     void multipleTest(){
-        Calculator calculator = new Calculator();
         int multiplication = calculator.multiply(30,20);
-
         assertEquals(600,multiplication);
     }
 
