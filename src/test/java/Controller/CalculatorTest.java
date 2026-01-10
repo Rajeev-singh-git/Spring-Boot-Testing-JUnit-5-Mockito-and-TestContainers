adda25bfb58e1ce00747709c7a0c8c27260a7b6e
Rajeev  Singh <rajeevsinghece49@gmail.com>
2026-01-10 09:54:26 +0530

    Display Test Name

--- src/test/java/Controller/CalculatorTest.java
+++ src/test/java/Controller/CalculatorTest.java
@@ -1,18 +1,30 @@
 package Controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Calculator Operation Test")
 class CalculatorTest {
 
     @Test
+    @DisplayName("Test addition of Two Numbers")
     public void addTest(){
 
         Calculator calculator = new Calculator();
         int actualResult  = calculator.add(10,20);
 
         assertEquals(30,actualResult);
+    }
+
+    @Test
+    @DisplayName("Test Subtraction")
+    void subtractTest(){
+        Calculator calculator = new Calculator();
+        int subtraction = calculator.subtract(30,20);
+
+        assertEquals(10,subtraction);
     }
 
 }
