3beb3d0a624a28fa7a478ce2a188f12f52ac367c
Rajeev  Singh <rajeevsinghece49@gmail.com>
2026-01-10 20:00:10 +0530

    ParameterizedTest : @ArgumentsSource

---
+++ src/test/java/parameterized/ArgumentsProviderDemoTest.java
@@ -0,0 +1,17 @@
+package parameterized;
+
+import Controller.MathUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArgumentsProviderDemoTest {
+
+    @ParameterizedTest
+    @ArgumentsSource(FactorialArgumentsProvider.class)
+    void factorialTest(int number, int expected){
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(expected, mathUtils.factorial(number));
+    }
+}
